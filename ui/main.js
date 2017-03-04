106754c88da779cb823fb45269f836f50a3a9e98
Rubinadesicrew <rubina@desicrew.in>
2017-03-04 14:51:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function () {
 };
 
 // Submit name
-var nameInput = document.getElememtById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
