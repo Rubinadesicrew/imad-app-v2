8e3a7dfc61685389afe4976d807b259b6da66033
Rubinadesicrew <rubina@desicrew.in>
2017-03-09 14:23:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ request.send(null);
 };
 
 // Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
 
@@ -54,6 +53,8 @@ ul.innerHTML = list;
 };
 
 // Make the request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 request.open('GET', 'http://rubinadesicrew.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
 };
