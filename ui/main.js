b88d263c468143936ab1f2c3888c76d25b6c455f
Rubinadesicrew <rubina@desicrew.in>
2017-03-09 14:14:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,59 @@
 // Counter code
 var button = document.getElementById('counter');
 
-button.onclick = function () {
-  
-  // Create a request object
-  var request = new XMLHttpRequest();
-  
-  // Capture the response and store it in a variable
-  request.onreadystatechange = function () {
-  if (request.readyState === XMLHttpRequest.DONE) {
-    // Take some action
-    if (request.status === 200) {
-        var counter = request.responseText;
-        var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-    }
-  } 
-  // Not done yet
-  };
-  // Make the request
-  request.open('GET', 'http://rubinadesicrew.imad.hasura-app.io/counter', true);
-  request.send(null);
+button.onclick = function(){
+
+// Create a request object
+var request = new XMLHttpRequest();
+
+// Capture the response and store it in a variable
+request.onreadystatechange = function() {
+if (request.readyState === XMLHttpRequest.DONE){
+// Take some action
+if (request.status === 200) {
+var counter = request.responseText;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+}
+}
+// Not done yet
 };
 
-// Submit name
+// Make the request
+request.open('GET', 'http://rubinadesicrew.imad.hasura-app.io/counter', true);
+request.send(null);
+};
+
+// Submit Name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    // Make a request to the server and send the name
-    
-    // Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    
-}; 
+
+// Create a request object
+var request = new XMLHttpRequest();
+
+// Capture the response and store it in a variable
+request.onreadystatechange = function() {
+if (request.readyState === XMLHttpRequest.DONE){
+// Take some action
+if (request.status === 200) {
+// Capture a list of names and render it as a list
+var names = request.responseText;
+names = JSON.parse(names);
+var list = '';
+for (var i=0; i<names.length; i++){
+list += '<li>'+ names[i] + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+
+}
+}
+// Not done yet
+};
+
+// Make the request
+request.open('GET', 'http://rubinadesicrew.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+};
