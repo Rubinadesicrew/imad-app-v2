73e3d4fa865d6192008f80300dcd0a975f5d4c85
Rubinadesicrew <rubina@desicrew.in>
2017-03-16 08:44:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,38 +14,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articles = {
-'article-one':  {
-    title: 'Article one Rubina Sheikh',
-    heading:'Article one',
-    date: 'Feb 16, 2017',
-    content: ` <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p> 
-             <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-             <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>`
-},
-'article-two': {
-    title: 'Article Two Rubina Sheikh',
-    heading:'Article Two',
-    date: 'Feb 16, 2017',
-    content: ` <p> 
-            This is the content for my second article.
-            </p>`
-},
-'article-three': {
-    title: 'Article Three Rubina Sheikh',
-    heading:'Article Three',
-    date: 'Feb 16, 2017',
-    content: ` <p> 
-            This is the content for my third article.
-            </p>`
-}
-};
+
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
